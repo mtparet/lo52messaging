4d823cc5b6823b9ead5607cc65fb788e2a7e1868
Francois Laithier <francois.laithier@gmail.com>
2012-05-01 23:12:51 +0200

    Base files of the project

---
+++ src/lo52/messaging/LO52MessagingActivity.java
@@ -0,0 +1,13 @@
+package lo52.messaging;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LO52MessagingActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

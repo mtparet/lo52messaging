ac5ab1cac91f919ebc161bca85a927d49c92ce20
Matthieu Paret <matthieuparet69@gmail.com>
2012-05-02 18:05:42 +0200

    debut de broadcast

--- src/lo52/messaging/LO52MessagingActivity.java
+++ src/lo52/messaging/LO52MessagingActivity.java
@@ -1,13 +1,45 @@
 package lo52.messaging;
 
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 
 public class LO52MessagingActivity extends Activity {
     /** Called when the activity is first created. */
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    private BroadcastReceiver fromService = new BroadcastReceiver(){
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// TODO Auto-generated method stub
+			
+		}
+    	
+    };
+    
+    @Override
+    protected void onResume() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(SendBroadcast.BROADCAST_ACTION);
+        registerReceiver(fromService, filter);
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(fromService);
+        super.onPause();
+    }
+
 }

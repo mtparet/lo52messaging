a7d2a4f41cf84662593ff86b8c3fd626e6e50793
Matthieu Paret <matthieuparet69@gmail.com>
2012-05-29 10:35:33 +0200

    séparation de MPagerAdapater dans un autre fichiers pour plus de clarté

--- src/lo52/messaging/activities/ConversationPagerActivity.java
+++ src/lo52/messaging/activities/ConversationPagerActivity.java
@@ -212,69 +212,7 @@ public class ConversationPagerActivity extends FragmentActivity implements TabHo
 	}
 
 
-	/**
-	 * The <code>PagerAdapter</code> serves the fragments when paging.
-	 */
-	public class MPagerAdapter extends FragmentPagerAdapter {
 
-		private List<Fragment> fragments;
-
-		/**
-		 * @param fm
-		 * @param fragments
-		 */
-		public MPagerAdapter(FragmentManager fm, List<Fragment> fragments) {
-			super(fm);
-			this.fragments = fragments;
-		}
-
-		/* (non-Javadoc)
-		 * @see android.support.v4.app.FragmentPagerAdapter#getItem(int)
-		 */
-		@Override
-		public Fragment getItem(int position) {
-			return this.fragments.get(position);
-		}
-
-		/* (non-Javadoc)
-		 * @see android.support.v4.view.PagerAdapter#getCount()
-		 */
-		@Override
-		public int getCount() {
-			return this.fragments.size();
-		}
-
-		public int getItemPosition(Object object) {
-			return POSITION_NONE;
-		}
-
-		/**
-		 * Retourne la liste des fragments dans l'adapter
-		 * @return
-		 */
-		public List<Fragment> getFragmentsList() {
-			return fragments;
-		}
-
-		/**
-		 * Ajoute un fragment dans l'adapter
-		 * @param f
-		 */
-		public void addFragment(Context ctx) {
-			ConversationFragment f = (ConversationFragment) Fragment.instantiate(ctx, ConversationFragment.class.getName());
-			fragments.add(f);
-		}
-
-		/**
-		 * Surcharge de destroyItem, dans laquelle on n'appelle pas la méthode super, sinon les fragments
-		 * sont resettés à leur View par défaut par le GC.
-		 */
-		@Override
-		public void destroyItem(ViewGroup container, int position, Object object) {
-			super.destroyItem(container, position, object);
-			Log.d(TAG, "Destroying fragment " + position);
-		}
-	}
 
 	/**
 	 * Création du menu d'options. Crafté à la main ici et non pas selon un .xml car le menu varie selon le nombre de conversations ouvertes
